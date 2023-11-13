eb0b4dde00a54a0b86fd6c049bcb2652a4960970
LinuxKunaL <kunallokhande774@gmail.com>
2023-11-13 12:41:54 +0530

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,23 +3,8 @@ import cors from "cors";
 import Config from "../config/index.js";
 import RouterMiddleware from "./middlewares/RouterMiddleware.js";
 import MainMiddleware from "./middlewares/MainMiddleware.js";
-import path, { fileURLToPath } from "url";
-import { dirname, join } from "path";
-import ejs from 'ejs'; 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+ 
 const app = express();
-app.set('view engine', 'ejs');
-
-app.use(express.static(join(__dirname, "client/build")));
-
-app.get('/', (req, res) => {
-  // Assuming your HTML file is named "index.html" in the "views" directory
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
 
 //━━━━━━━━━━━━━━━━ Middleware ━━━━━━━━━━━━━━━━//
 MainMiddleware(app, cors, express);
